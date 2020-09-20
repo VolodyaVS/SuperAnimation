ac07db21905ffe809a202ed69ae135bfd4feb211
Volodya <volodyavolga@protonmail.ch>
2020-09-20 11:31:44 +0300

    test

---
+++ SuperAnimation/SupportingFiles/SceneDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.swift
+//  SuperAnimation
+//
+//  Created by Vladimir Stepanchikov on 19.09.2020.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+}
+
